d2aa3de4abe8df13647b0ef3f256f9da05e920d0
ikhsan <ikhsanudinhakim@gmail.com>
2022-06-06 16:56:54 +0700

    Initial commit

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Component } from 'react/cjs/react.production.min'
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasError: false,
+    }
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true })
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Oops, something went wrong</h1>
+    } else {
+      return this.props.children
+    }
+  }
+}
+
+export default ErrorBoundary
