75c7c7f7a488ecba3d1b9aefdea14218f7ef0672
ikhsan <ikhsanudinhakim@gmail.com>
2022-06-06 17:09:27 +0700

    Updates

---
+++ precache-manifest.fe7a93c04daa36a68905c729a7eb9c28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4e4a85fdc39b239b210",
+    "url": "/robofriends/static/css/main.a1cc64d1.chunk.css"
+  },
+  {
+    "revision": "d4e4a85fdc39b239b210",
+    "url": "/robofriends/static/js/main.7098180a.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "274cfe22221aaa5a6961",
+    "url": "/robofriends/static/css/2.1ba945f6.chunk.css"
+  },
+  {
+    "revision": "274cfe22221aaa5a6961",
+    "url": "/robofriends/static/js/2.193202e4.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "636e468456d990f8a284e17bcad09930",
+    "url": "/robofriends/index.html"
+  }
+];
